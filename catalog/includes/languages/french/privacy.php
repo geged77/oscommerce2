3cfba677b7e3e3ea464ae9a478cbc30f29615a7e
Laurent <foxp2projects@googlemail.com>
2010-11-13 16:12:08 +0100

    traduction des pages info_shopping_cart -> privacy

---
+++ catalog/includes/languages/french/privacy.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Remarque sur la confidentialité');
+define('HEADING_TITLE', 'Remarque sur la confidentialité');
+
+define('TEXT_INFORMATION', 'Insérer ici vos remarques confidentialité.');
+?>
