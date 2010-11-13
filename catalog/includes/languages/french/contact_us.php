fd05fbe648f3fcd460e6e0fcfc41b3ed8cc84022
Laurent <foxp2projects@googlemail.com>
2010-11-13 15:58:40 +0100

    traduction des pages conditions -> index

---
+++ catalog/includes/languages/french/contact_us.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Contactez-nous');
+define('NAVBAR_TITLE', 'Contactez-nous');
+define('TEXT_SUCCESS', 'Votre requête a été envoyée avec succès au propriétaire du magasin.');
+define('EMAIL_SUBJECT', 'Requête de ' . STORE_NAME);
+
+define('ENTRY_NAME', 'Nom et Prénom :');
+define('ENTRY_EMAIL', 'Adresse email :');
+define('ENTRY_ENQUIRY', 'Demande de renseignements :');
+
+define('ERROR_ACTION_RECORDER', 'Erreur: Une requête a été déjà envoyée.Merci d\'essayer à nouveau dans %s minutes.');
+?>
---
+++ catalog/includes/languages/french/create_account_success.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Créer un comtpe');
+define('NAVBAR_TITLE_2', 'Succès');
+define('HEADING_TITLE', 'Votre compte a été créé !');
+define('TEXT_ACCOUNT_CREATED', 'Félicitations ! Votre nouveau compte a été créé avec succès ! Vous pouvez maintenant profiter des privilèges de membre afin de tirer pleinement parti de notre magasin en ligne. Pour <small><strong>TOUTE</strong></small> question sur le fonctionnement de ce magasin en ligne, envoyez s\'il vous plaît un courrier électronique au <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">propriétaire du magasin</a>.<br /><br />Une confirmation a été envoyée à l\'adresse électronique fournie. Si vous ne l\'avez pas reçu dans l\'heure, s\'il vous plaît  <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">contactez-nous</a>.');
+?>
