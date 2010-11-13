119c9fc36d0d7c23a1cd3c2dcd2522fa0a59b812
Laurent <foxp2projects@googlemail.com>
2010-11-13 12:07:14 +0100

    traduction du repertoire modules boxes

---
+++ catalog/includes/languages/french/modules/boxes/bm_product_notifications.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_TITLE', 'Notifications');
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_DESCRIPTION', 'Affiche les notifications des produits sur la page produit.');
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_TITLE', 'Notifications');
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_NOTIFY', 'Me prévenir des mises à jour de <strong>%s</strong>');
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_NOTIFY_REMOVE', 'Ne plus me prévenir des mises à jour de <strong>%s</strong>');
+?>
