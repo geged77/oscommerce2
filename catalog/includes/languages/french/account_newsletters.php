435a3bb73b0d7771349617fc5f355f2176ffb230
Laurent <foxp2projects@googlemail.com>
2010-11-13 14:54:23 +0100

    traduction des pages account

---
+++ catalog/includes/languages/french/account_newsletters.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2003 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mon compte');
+define('NAVBAR_TITLE_2', 'Souscriptions bulletin d\'information');
+
+define('HEADING_TITLE', 'Souscriptions bulletin d\'information');
+
+define('MY_NEWSLETTERS_TITLE', 'Mes souscriptions bulletin d\'informations');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Bulletin d\'informations général');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Y compris les actualités du magasin, les nouveaux produits, les offres spéciales, et les autres annonces promotionnelles.');
+
+define('SUCCESS_NEWSLETTER_UPDATED', 'Vos abonnements au bulletin d\'informations ont été mis à jour avec succès.');
+?>
