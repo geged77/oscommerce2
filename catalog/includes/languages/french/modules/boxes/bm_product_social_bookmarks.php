975a12086c8d173feaeb1cf62641b9c4d0ad03a5
gnidhal <gnidhal@free.fr>
2010-11-18 23:07:28 +0100

    Traduction module Bookmark social correction module box manufacturer Utf-8

--- catalog/includes/languages/french/modules/boxes/bm_product_social_bookmarks.php
+++ catalog/includes/languages/french/modules/boxes/bm_product_social_bookmarks.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_BOXES_PRODUCT_SOCIAL_BOOKMARKS_TITLE', 'Partager ce produit:');
-  define('MODULE_BOXES_PRODUCT_SOCIAL_BOOKMARKS_DESCRIPTION', 'Affiche les favoris sociaux sur la page d\'informations produit');
+  define('MODULE_BOXES_PRODUCT_SOCIAL_BOOKMARKS_TITLE', 'Partage réseaux sociaux');
+  define('MODULE_BOXES_PRODUCT_SOCIAL_BOOKMARKS_DESCRIPTION', 'Affiche le bloc réseaux sociaux sur la page d\'informations produit');
   define('MODULE_BOXES_PRODUCT_SOCIAL_BOOKMARKS_BOX_TITLE', 'Partager un produit');
 ?>
